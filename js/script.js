7f6a3457c42ba6a4d17e5e7ca24be6c216d56c8c
mesciuS <giovadev@gmail.com>
2023-02-27 16:34:42 +0100

    feat: esercizio uno

--- js/script.js
+++ js/script.js
@@ -5,25 +5,34 @@
 
 let myArray = ["mail@uno.com", "mail@due.com", "mail@tre.com", "mail@quattro.com"];
 
-for(let i = 0; i < myArray.length; i++) { 
-
-    console.log(myArray[i])
-}
-
-
-
 let userEmail = document.getElementById("emailEl");
 
 let userInput = document.getElementById("btnEl");
 
+let resultEl = document.getElementById("result");
+
 userInput.addEventListener("click", function() {
+
+    // al di fuori del for devo dichiarare una variabile che possa avere due valori in base all'if
+    let found = false;
     
-    
-    if(userEmail == myArray[i]) {
-        
-        console.log("ciao");
-        
+    // il for va dichiarato all'interno dell'event listener
+    for(let i = 0; i < myArray.length; i++) { 
+
+
+        if(userEmail.value == myArray[i]) { 
+            // questo è il secondo valore della variabile quando l'email è una dell'array
+            found = true;                
+        }
     }
     
+
+    // gli if/else per il risultato finale dovranno essere dichiarati fuori dal for ma dentro l'event
+    if(found == true) {
+        resultEl.innerHTML = ("ACCESS GRANTED")
+    }
+    else {
+        resultEl.innerHTML = ("ACCESS DENIED")
+    }
 
 });
